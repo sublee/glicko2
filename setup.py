4a3078e52d374f15aabfc88ad44a4c3a6ba4fdbe
Heungsub Lee <sub@subl.ee>
2016-08-16 10:51:29 +0900

    Add LICENSE file

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import with_statement
+
 import re
+
 from setuptools import setup
 from setuptools.command.test import test
-import sys
 
 
 # detect the current version
@@ -24,12 +25,12 @@ setup(
     version=version,
     license='BSD',
     author='Heungsub Lee',
-    author_email='h'r'@'r's'r'u'r'b'r'l'r'.'r'e'r'e',
+    author_email='s'r'u'r'b'r'@'r's'r'u'r'b'r'l'r'.'r'e'r'e',
     url='http://github.com/sublee/glicko2',
     description='The Glicko-2 rating system',
     long_description=__doc__,
     platforms='any',
-    py_modules=[ 'glicko2'],
+    py_modules=['glicko2'],
     classifiers=['Development Status :: 1 - Planning',
                  'Intended Audience :: Developers',
                  'Intended Audience :: Science/Research',
